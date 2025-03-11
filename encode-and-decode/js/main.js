42a39947e515b37212bc42708508f75a1eb607ad
kyle <nojaim.k@gmail.com>
2025-03-10 20:27:30 -0400

    added handling for empty arrays and strings

--- encode-and-decode/js/main.js
+++ encode-and-decode/js/main.js
@@ -1,22 +1,26 @@
 class Solution {
-    /**
+    /*
      * @param {string[]} strs
      * @returns {string}
      */
     encode(strs) {
-        return strs.map(str => {
+        if (!strs.length) return '';
+        const coded = strs.map(str => {
             return str.split('').map(char =>{
                 const num = char.charCodeAt(0);
                 return String.fromCharCode(num + 10);
             }).join('')
         }).join('poopy');
+        return coded === '' ? 'handle' : coded;
     }
 
-    /**
+    /*
      * @param {string} str
      * @returns {string[]}
      */
     decode(str) {
+        if (!str) return [];
+        if (str === 'handle') return [''];
         return str.split('poopy').map(word => {
             return word.split('').map(char => {
                 const num = char.charCodeAt(0);
@@ -25,6 +29,7 @@ class Solution {
         })
     }
 }
+
 
 
 //Input: ["neet","code","love","you"]
