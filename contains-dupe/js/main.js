17a15040bcefcb94c1eebf195083d848f39ec19b
kyle <nojaim.k@gmail.com>
2025-03-14 15:48:17 -0400

    better time complexity on valid anagram

--- contains-dupe/js/main.js
+++ contains-dupe/js/main.js
@@ -11,3 +11,49 @@ class Solution {
         // return new Set(nums).size !== nums.length;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+// Example 1:
+
+// Input: nums = [1, 2, 3, 3]
+
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1, 2, 3, 4]
+
+// Output: false
+
+
+
+// params : array [] numbers
+
+// return boolean
+
+// examples : see above
+
+// get a new array, containing no duplicates, compare length of that array to the original
+
+function hasDupes(arr){
+    const noDupes = new Set(arr);
+    return noDupes.size !== arr.length;
+}
--- valid-anagram/js/main.js
+++ valid-anagram/js/main.js
@@ -9,3 +9,78 @@ class Solution {
                t.split('').sort().join(''); 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+// An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+// Example 1:
+
+// Input: s = "racecar", t = "carrace"
+
+// Output: true
+// Example 2:
+
+// Input: s = "jar", t = "jam"
+
+// Output: false
+// Constraints:
+
+// s and t consist of lowercase English letters.
+
+
+// params: 2 strings - all lowercase
+
+// return : boolean - true or false
+
+// examples: 'racecar' + 'carrace' -> true (each has rx2, ax2, cx2, ex1)
+                // 'jam' + 'jar' -> false (each has jx1, ax1), but one has an r, and one has a an m
+
+// pseudo - by sorting each word, all characters should be placed in a specific order (basically creating a key for each combination of letters)
+
+function isAnagram(s,t){
+    // return s.split('').sort().join('') === t.split('').sort().join('');
+
+    if (s.length !== t.length) return false;
+    const sTot = s.split('').reduce((a,el) => {
+        a[el] = (a[el] || 0) + 1;
+        return a;
+    }, {});
+    const tTot = t.split('').reduce((a,el) => {
+        a[el] = (a[el] || 0) + 1;
+        return a;
+    }, {});
+    for (let key in sTot){
+        if (sTot[key] !== tTot[key]) return false;
+    }
+    return true;
+}
