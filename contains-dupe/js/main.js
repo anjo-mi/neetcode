b487db1ed3436bc086e29b40513db281d9517177
kyle <nojaim.k@gmail.com>
2025-03-14 14:19:37 -0400

    alternate approach for containing duplicates

---
+++ contains-dupe/js/main.js
@@ -0,0 +1,13 @@
+class Solution {
+    /*
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const nonDupes = Array.from(new Set(nums))
+        return nonDupes.length !== nums.length
+
+        // alt
+        // return new Set(nums).size === nums.length;
+    }
+}
